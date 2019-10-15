fadb84881e217c9b9055cfc7f18d795e9a3b9d22
Victoria Hale <vic.hale74@gmail.com>
2019-10-15 00:08:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,9 @@
  class Dog 
-   
-  def initialize(name="Mutt")
-    @name = name
-   
-  end
-  def name 
-    @name 
-  end
-
-end 
+   def initialize(name)
+     @name = name
+   end 
+   def name 
+     @name
+   end
+ end 
+  
